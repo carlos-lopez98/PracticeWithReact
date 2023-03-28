afad846763ca994f8b655aa35a917d0fc4d3007f
Carlos <cjimenez9811@gmail.com>
2023-03-28 15:47:09 -0700

    Renders String on Button Click

--- src/components/RenderString.jsx
+++ src/components/RenderString.jsx
@@ -6,9 +6,24 @@ export const RenderString = () => {
 
     const [textInput, setTextInput] = useState("")
 
+    const [output, setOutput] = useState("") 
+
     /* Function updates textInput to equal my newInput being passed in */
-    function handleTextInputChange(newInput){
-        setTextInput(newInput)
+    function handleTextInputChange(event){
+        setTextInput(event.target.value)
+    }
+    /* Solution is to have an input and output variable, output variable is assigned on buttonclick */
+    function onbuttonClick(){
+      setOutput(textInput)
+    }
+
+    /* This will always render what the value for textInput is */
+    function RenderString({output}){
+        return (
+            <ul>
+                <li>{output}</li>
+            </ul>
+        )
     }
 
   return (
@@ -17,9 +32,14 @@ export const RenderString = () => {
         <div className='TextInputContainer'>
             <h1>Enter Text</h1>
             {/* The way I'm using the onChangeListener is by callback function aka anonymously */}
-            <input type="text" value={textInput} onChange= {() => {
-                handleTextInputChange(textInput)}}/>
+            <input type="text" value={textInput} onChange={handleTextInputChange}></input>
+            <button onClick={() => {
+                onbuttonClick()
+            }}>Click Me To Change Below Text!</button>   
+
         </div>
+
+       <RenderString output={output}/>
         </div>
   )
 }
